27f535a8f8630d7d86eedb5e053e736e4663503b
Ale Cunha <ale@rcunha.net>
2020-05-15 09:02:11 -0500

    Change version to 1.0.5 (preparing release)

--- lilvlib/__init__.py
+++ lilvlib/__init__.py
@@ -2,4 +2,4 @@ from lilvlib.lilvlib import (
     get_pedalboard_info, get_pedalboard_name, plugin_has_modgui, get_plugin_info, get_plugins_info, get_bundle_dirname, NS
 )
 
-__version__ = '1.0.5a1'
+__version__ = '1.0.5'
