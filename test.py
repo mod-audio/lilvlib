8219ff653583477c7363b4dd8aad787870b50057
falkTX <falktx@falktx.com>
2026-01-18 10:47:53 +0100

    Make test output easier to read, allow to pass bundles to inspect
    Signed-off-by: falkTX <falktx@falktx.com>

--- test.py
+++ test.py
@@ -2,9 +2,20 @@
 
 import lilv
 import lilvlib
+import os
+import sys
 
 world = lilv.World()
-world.load_all()
+
+if len(sys.argv) > 1:
+    world.load_specifications()
+    world.load_plugin_classes()
+    for bundle in sys.argv[1:]:
+        if not bundle.endswith(os.sep):
+            bundle += os.sep
+        world.load_bundle(world.new_file_uri(None, bundle))
+else:
+    world.load_all()
 
 for plugin in world.get_all_plugins():
     info = lilvlib.get_plugin_info(world, plugin)
@@ -23,5 +34,5 @@ for plugin in world.get_all_plugins():
         errors.remove('plugin is missing version information')
 
     if errors:
-        print("----------------------", str(plugin.get_uri()))
-        print(errors)
+        print("----------------------", str(plugin.get_uri()), "----------------------")
+        print('\t -> ' + '\n\t -> '.join(errors))
